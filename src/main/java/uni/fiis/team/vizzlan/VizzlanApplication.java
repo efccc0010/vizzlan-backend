ce4da0aca663a7ae62cafd891bf2b4fbb3fcedb1
fernandoedgar <edgar.carrion.c@uni.pe>
2021-11-22 19:18:32 -0500

    Primer commit

---
+++ src/main/java/uni/fiis/team/vizzlan/VizzlanApplication.java
@@ -0,0 +1,13 @@
+package uni.fiis.team.vizzlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizzlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizzlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/fiis/team/vizzlan/VizzlanApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.fiis.team.vizzlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizzlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
