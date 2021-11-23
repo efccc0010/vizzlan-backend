044fc4c4041a54d7fb4cc6fa8d0659fa6a7393f7
kchal11 <kevin.chirihuana.a@uni.pe>
2021-11-23 18:24:36 -0500

    pruebas kevin

--- src/main/java/uni/fiis/team/vizzlan/service/productoService/ProductoService.java
+++ src/main/java/uni/fiis/team/vizzlan/service/productoService/ProductoService.java
@@ -1,4 +1,11 @@
 package uni.fiis.team.vizzlan.service.productoService;
 
 public class ProductoService {
+    private static Integer id = 0;
+    public ProductoService(){
+        this.id +=1;
+    }
+    public Integer getId(){
+        return this.id;
+    }
 }
