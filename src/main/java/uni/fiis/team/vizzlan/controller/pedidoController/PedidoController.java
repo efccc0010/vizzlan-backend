e673d6ccab17f32f8eca1d32a6f29c4f2f824a0e
kchal11 <kevin.chirihuana.a@uni.pe>
2021-11-26 08:54:42 -0500

    pedidos modificacion controller y propiedades

--- src/main/java/uni/fiis/team/vizzlan/controller/pedidoController/PedidoController.java
+++ src/main/java/uni/fiis/team/vizzlan/controller/pedidoController/PedidoController.java
@@ -1,6 +1,11 @@
 package uni.fiis.team.vizzlan.controller.pedidoController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import uni.fiis.team.vizzlan.domain.pedido.PedidoNormal;
+import uni.fiis.team.vizzlan.service.pedidoService.PedidoService;
 
 @RestController
 public class PedidoController {
@@ -9,7 +14,10 @@ public class PedidoController {
     PedidoService pedidoService;
     
     @PostMapping("/registrarPedido")
-    public String registroPedido(@RequestBody PedidoRequest){
+    public String registroPedido(@RequestBody PedidoRequest pr) throws Exception{    
+        PedidoNormal p = new PedidoNormal(pr.getCodigo(),pr.getFechaPedido(),pr.getDescripcion());
+        pedidoService.realizarPedidoInicio(p);
+        return "Ingreso Dado";
         
     }
 }
---
+++ src/main/java/uni/fiis/team/vizzlan/controller/pedidoController/PedidoRequest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uni.fiis.team.vizzlan.controller.pedidoController;
+
+import java.util.Date;
+
+/**
+ *
+ * @author chiri
+ */
+class PedidoRequest {
+        private Integer codigo ;
+        private Date fechaPedido;
+        private String descripcion;
+
+    public PedidoRequest(Integer codigo, Date fechaPedido, String descripcion) {
+        this.codigo = codigo;
+        this.fechaPedido = fechaPedido;
+        this.descripcion = descripcion;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getFechaPedido() {
+        return fechaPedido;
+    }
+
+    public void setFechaPedido(Date fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+        
+        
+}
